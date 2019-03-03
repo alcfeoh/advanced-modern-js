c3e62997e81ebd8aa5480ed6713ba9ff99dde76b
Alain Chautard <alain@interstate21.com>
2019-03-03 13:02:08 -0800

    Better solution for ex 01

--- exercises/ex-01/end/utils.js
+++ exercises/ex-01/end/utils.js
@@ -1,12 +1,9 @@
-let target = document.getElementById( 'output' );
+const target = document.getElementById( 'output' );
 
-const logger = {
+export const logger = {
   log: function (msg) {
-    let msgs = target.innerHTML.split('<br>');
-    msgs = msgs.slice(-2);
+    const msgs = target.innerHTML.split('<br>');
     msgs.push(msg);
     target.innerHTML = msgs.join('<br>');
   }
-}
-
-export { logger };
+};
