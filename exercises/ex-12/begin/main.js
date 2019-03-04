c4e0c746ab0ddec68497e02baad6154c58a464bf
Alain Chautard <alain@interstate21.com>
2019-03-04 13:52:32 -0800

    Updates on ex13 instructions

--- exercises/ex-12/begin/main.js
+++ exercises/ex-12/begin/main.js
@@ -3,6 +3,5 @@ console.log( 'Just a test, for the START of the exercise' );
 import { people } from './people.js';
 import { logger } from './utils.js';
 
-window.addEventListener( 'DOMContentLoaded', () => {
   people.forEach( logger.personLog );
-} );
+
--- exercises/ex-13/ex-13-instructions.md
+++ exercises/ex-13/ex-13-instructions.md
@@ -12,6 +12,7 @@ It should have the following fields:
 * employeeId (hidden, non-enumerable, as `id` in `Person`)
 * jobTitle
 * department
+
 It should override `Person`'s `toString` method to use the new properties `jobTitle`
 and `department`.  
  
