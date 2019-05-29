c0b305ed29b9807e057904dc2c5c53a33be4955f
Alain Chautard <achautard@gmail.com>
2019-05-29 12:51:14 -0700

    Fixed error

--- exercises/ex-05/end/utils.js
+++ exercises/ex-05/end/utils.js
@@ -22,7 +22,7 @@ const logger = {
 
     for ( let i = 0; i < keys.length; i++ ) {
       let key = keys[ i ];
-      if ( [ 'function, object' ].indexOf( typeof obj[ key ] ) ) {
+      if ( [ 'function', 'object' ].indexOf( typeof obj[ key ] ) ) {
         html += `<li>${key} : ${obj[ key ]}</li>`;
       }
     }
